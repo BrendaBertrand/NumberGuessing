4c39c87746a5ee7483a2bd1a4e6b9d2a8af81739
Brenda Bertrand <brenda.bertrand.b@gmail.com>
2024-04-09 07:17:19 +0200

    Refactoring and fix of the close case

--- NumberGuessing/Program.cs
+++ NumberGuessing/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     const int MIN_VALUE = 0;
     const int MAX_VALUE = 100;
-    const int MAX_ATTEMPTS = 3;
+    const int MAX_ATTEMPTS = 5;
 
     static void Main(string[] args)
     {
@@ -13,32 +13,25 @@ class Program
 
         Console.WriteLine("Hello, Welcome to our Number Guessing Game!");
         // The magicNumber should be removed from this message in a real game 
-        Console.WriteLine($"The computer choose an integer number between {MIN_VALUE} and {MAX_VALUE} and it's {magicNumber}"); 
+        Console.WriteLine(
+            $"The computer choose an integer number between {MIN_VALUE} and {MAX_VALUE} and it's {magicNumber}");
         Console.WriteLine($"Will you find it in {MAX_ATTEMPTS} attempts maximum ?");
         int userGuess = -1;
-        int attempt = 1;
+        int attempt = 0;
 
         do
         {
             bool isParseSuccessful = false;
-            Console.Write($"\nWhat's your guess ? \nAttempt n°{attempt} : ");
+            Console.Write($"\nWhat's your guess ? \nAttempt n°{attempt + 1} : ");
             isParseSuccessful = Int32.TryParse(Console.ReadLine(), out userGuess);
 
-            if (isParseSuccessful)
+            if (isParseSuccessful && userGuess >= MIN_VALUE && userGuess <= MAX_VALUE)
             {
-                if (userGuess > magicNumber)
+                attempt++;
+                if (userGuess == magicNumber)
                 {
-                    Console.WriteLine("Your number is too high! ");
-                    attempt++;
-                }
-                else if (userGuess < magicNumber)
-                {
-                    Console.WriteLine("Your number is too low! ");
-                    attempt++;
-                }
-                else
-                {
-                    Console.Write($"Congratulations ! You found the Magic Number {magicNumber} in {attempt} attempt");
+                    Console.Write(
+                        $"Congratulations ! You found the Magic Number {magicNumber} in {attempt} attempt");
                     if (attempt > 1)
                     {
                         Console.Write("s");
@@ -46,18 +39,31 @@ class Program
 
                     break;
                 }
-
                 if (Math.Abs(userGuess - magicNumber) <= 5)
                 {
                     Console.WriteLine("You're close!");
                 }
+                else
+                {
+                    if (userGuess > magicNumber)
+                    {
+                        Console.WriteLine("Your number is too high! ");
+                    }
+
+                    if (userGuess < magicNumber)
+                    {
+                        Console.WriteLine("Your number is too low! ");
+                    }
+                   
+                }
             }
-            else
+
+            if (!isParseSuccessful || userGuess < MIN_VALUE || userGuess > MAX_VALUE)
             {
                 Console.WriteLine($"Please enter an integer between {MIN_VALUE} and {MAX_VALUE}.");
             }
 
-            if (attempt > MAX_ATTEMPTS)
+            if (attempt >= MAX_ATTEMPTS)
             {
                 Console.WriteLine("No more attempts available -> You Lose ");
                 break;
--- NumberGuessing/obj/Debug/net8.0/NumberGuessing.AssemblyInfo.cs
+++ NumberGuessing/obj/Debug/net8.0/NumberGuessing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumberGuessing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebee86bd925fdc4928cd47fb2c74a7baf15de583")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc68921e5612a502a9675c1e7b5a35e259025110")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumberGuessing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumberGuessing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
